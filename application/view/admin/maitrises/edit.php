c090e8d2f8c9502b8833409d400a37b54b458b50
Thibaud Blanchard <dentico.thibaud@gmail.com>
2021-07-22 00:32:16 +0200

    edit -> cetait un add enfaite

--- application/view/admin/maitrises/edit.php
+++ application/view/admin/maitrises/edit.php
@@ -1,7 +1,7 @@
 <a href="<?php echo HOST ?>admin/maitrises/list">Retour à la liste</a>
 <h1><?php echo $this->_data['label']; ?></h1>
 
-<form action="<?php echo HOST ?>admin/maitrises/save" method="POST">
+<form action="<?php echo HOST ?>admin/maitrises/save/<?php echo $this->_data['id']; ?>" method="POST">
 	<div>
 		<input name="label" id="label" type="text" placeholder="Nom" value="<?php echo $this->_data['label']; ?>" /><br/>
 		<textarea name="description" id="description"><?php echo $this->_data['description']; ?></textarea><br /><br/>
